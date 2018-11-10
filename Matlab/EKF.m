5f4db3a9de8ed31e17340cf13518b2afa139714b
silvaordie <40876673+silvaordie@users.noreply.github.com>
2018-11-10 15:12:29 +0000

    Perfeito

--- Matlab/EKF.m
+++ Matlab/EKF.m
@@ -9,8 +9,8 @@ x=zeros(6+2*LANDMARKS, T);
 
 x(:, 1) = [0; 0; 0; 0; 0; 0; 1; 2; 0; 2; -2; 0];
 
-xreal= [0.1*ts ; 0.2*ts];
-cov=diag([ 1 1 1 1 1 1 2*ones(1, 2*LANDMARKS)]);
+xreal= [1-cos(ts) ; sin(ts)];
+cov=diag([ 0 0 0 0 0 0 1 2*zeros(1, 2*LANDMARKS-1)]);
 
 F=eye(6+2*LANDMARKS);
 F(1,4)=1;
@@ -23,12 +23,13 @@ for t=2:1:T
    xp(1:3)=x(1:3, t-1)+x(4:6,t-1);
    xp(4:6)=x(4:6,t-1);
    xp(7:6+2*LANDMARKS)=x(7:6+2*LANDMARKS,t-1);
-   covp=F*cov*F'+diag([0.5 0.5 pi/10 0.2 0.2 0.2 0 0 0 0 0 0 ]);
+   covp=F*cov*F'+diag([10 10 pi/10 1 1 1 0 0 0 0 0 0 ]);
    
    %Update
    z=obs(xreal(:,t)',LS);
-   S=H(xp, LANDMARKS)*covp*H(xp, LANDMARKS)' + diag([0.3 0.3 0.3]);
+   S=H(xp, LANDMARKS)*covp*H(xp, LANDMARKS)' + diag([1 1 1]);
    K=covp*H(xp, LANDMARKS)'*inv(S);
    cov=covp-K*S*K';
    x(:,t)=xp'+K*(z-hp(xp));
+   
 end
