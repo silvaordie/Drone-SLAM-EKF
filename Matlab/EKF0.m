0f9392a00b6d89f56e462cdead478cc982983663
silvaordie <40876673+silvaordie@users.noreply.github.com>
2018-11-29 01:24:54 +0000

    Ideia

--- Matlab/EKF0.m
+++ Matlab/EKF0.m
@@ -1,40 +1,28 @@
 clear 
 
-V=[1; 2];
+V=[0.1; 0.2];
 x(1:2,1)=[0;0];
-x(1:2,1)=V;
-LS=[ 1 2 1 ; -1 2 1 ];
+LS=[ 6 2 10 ; -1 5 6 ];
 
 for k=2:1:50
     
    x(1:2,k)=x(1:2,k-1)+V;
-   dr(1,k)=norm(x(:,k)-LS(:,1));
-   dr(2,k)=norm(x(:,k)-LS(:,2));
-   dr(3,k)=norm(x(:,k)-LS(:,3));
+   first= [(x(:,k)-LS(:,1))' ; (x(:,k)-LS(:,2))' ; (x(:,k)-LS(:,3))' ];
+   dr(:,k) = [ first(1,1)^2 + first(1,2)^2 ; first(2,1)^2 + first(2,2)^2 ; first(3,1)^2 + first(3,2)^2 ];
+    
   
 end
 
-cvx_begin quiet
-
-variable v(2,1);
-variable L(2,3);
-xp(1:2,1)=[0;0];
-err=0;
-for k=2:1:50
-   d(1,k)=norm(xp-L(:,1));
-   d(2,k)=norm(xp-L(:,2));
-   d(3,k)=norm(xp-L(:,3));
-   
-   err(1)=err(1) + square_pos(norm(square_pos(d(1,k))-square_pos(dr(1,k))));
-   err(2)=err(2) + square_pos(norm(square_pos(d(2,k))-square_pos(dr(2,k))));
-   err(3)=err(3) + square_pos(norm(square_pos(d(3,k))-square_pos(dr(3,k))));
+for k=1:1:50
+    
+   xd(1:2,k)=x(1:2,k)+abs(0.1*rand(1,1)*V);
+   first= [(xd(:,k)-LS(:,1))' ; (xd(:,k)-LS(:,2))' ; (xd(:,k)-LS(:,3))' ];
+   d(:,k) = [ first(1,1)^2 + first(1,2)^2 ; first(2,1)^2 + first(2,2)^2 ; first(3,1)^2 + first(3,2)^2 ];
+    
 end
+v(1,:)=x(2,:);
+v(2,:)=x(1,:);
+z=d(1,:);
 
-min( sum(err) );
 
-for k=2:1:50
-      xp(1:2,k)  == xp(1:2,k-1)+v;  
-end
-
-xp(1:2,1)=[0 0]
-cvx_end;
+[lm, e]=trilat(x, d)
