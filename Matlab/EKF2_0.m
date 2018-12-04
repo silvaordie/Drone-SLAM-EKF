db479b0a72dc3b0fd869fe95f89516517f5e4140
silvaordie <jfsilva988@hotmail.com>
2018-12-04 18:08:33 +0000

    Angulos

--- Matlab/EKF2_0.m
+++ Matlab/EKF2_0.m
@@ -46,51 +46,51 @@ LANDMARKS = 3;
 %Simula a trajetória real
 ts = 1:1:T;
 xreal= [ -1.5*ones(size(ts)) + 1.5*cos(0.1*ts) ; 1.5*sin(0.1*ts)];
-
+areal= [ zeros(1,length(ts)); 0:pi/(6*(T-1)):pi/6 ; 0:pi/(T-1):pi ];
 %Vetor de estado ao longo do tempo
-x=zeros(6+2*LANDMARKS, T);
+x=zeros(10+2*LANDMARKS, T);
 %Vetor de observações ao longo do tempo
-z=zeros(3, T);
+z=zeros(3+LANDMARKS, T);
 
 %INICIALIZAÇÃO
 %Posição e velocidade do veículo a (0,0) e segunda landmark a (0,0)
 %Segunda Landmark Inicializada a (D,0)
 %Terceira Landmark inicializada com o resultado da trilateração
-x(:, 1) = [0; 0; 0; 0; 0; 0; D; 0; 0; 0; lm(1,1); lm(2,1)];
+x(:, 1) = [0; 0; 0; 0; D; 0; 0; 0; lm(1,1); lm(2,1) ; 0 ; 0 ; 0 ; 0 ; 0 ; 0];
 %Varânicia da posiçao , velocidade do veículo e da segunda landmark é nula (assume-se que está 100% correta)
 %Variancia do x primeira landmark assume-se a variancia das medições
 %Variância da 3ª landmark corresponde ao maior desvio quadrático proveniente da trilateração
-cov=diag([ 0 0 0 0 0 0 0 0.04 0 0 e^2*ones(1, 2)]);
+cov=diag([ 0 0 0 0 0.04 0 0 0 e^2*ones(1, 2) 0 0 0 0 0 0]);
 
 %Matriz F
-F=eye(6+2*LANDMARKS);
-F(1,4)=1;
-F(2,5)=1;
-F(3,6)=1;
+F=eye(10+2*LANDMARKS);
+F(1,3)=1;
+F(2,4)=1;
+F(length(x(:, 1))-5,length(x(:, 1))-2)=1;
+F(length(x(:, 1))-4,length(x(:, 1))-1)=1;
+F(length(x(:, 1))-3,length(x(:, 1)))=1;
 
-figure;
-hold on;
 for t=2:1:T
     
    
    %% Prediction
    %Assume um valor de velocidades parecido ao do instante anterior
-   xp(4:6)=x(4:6,t-1);
+   xp(3:4)=x(3:4,t-1);
    %Itera a posição com base na velocidade assumida
-   xp(1:3)=x(1:3, t-1)+xp(4:6)';
+   xp(1:2)=x(1:2, t-1)+xp(3:4)';
    %As landmarks não são afetadas pelo movimento do drone
-   xp(7:6+2*LANDMARKS)=x(7:6+2*LANDMARKS,t-1);
+   xp(5:10+2*LANDMARKS)=x(5:10+2*LANDMARKS,t-1);
    
    %Atualiza a matriz das covariancias
-   covp=F*cov*F'+diag([0.1 0.1 pi/10 0.1 0.10 0.10 0 0 0 0 0 0 ]);
+   covp=F*cov*F'+diag([0.1 0.1 0.1 0.1 0 0 0 0 0 0 pi/10 pi/10 pi/10 0.0001 0.0001 0.0001 ]);
    
    
    
    %% Update
    %Determina as dstâncias às landmarks (com erros maximos de 0.2)
-   z(:,t)=obs(xreal(:,t)',LS);
+   z(:,t)=obs(xreal(:,t)',LS, areal(:,k));
    %Determina o S 
-   S=H(xp, LANDMARKS)*covp*H(xp, LANDMARKS)' + diag([0.04 0.04 0.04]);
+   S=H(xp, LANDMARKS)*covp*H(xp, LANDMARKS)' + diag([0.04 0.04 0.04 0 0 0]);
    %Calcula o ganho de Kalman
    K=covp*H(xp, LANDMARKS)'*inv(S);
    %Corrige a matriz das covariâncias
@@ -101,14 +101,15 @@ for t=2:1:T
    
    i=z(:,t)-hp(xp);
    %Filtro de outliers
-   e=(abs(i)<5);
-   x(:,t)=xp'+(K*(e.*i));   
+   x(:,t)=xp'+(K*i);   
    %Filtro passa baixo para suavizar o sinal
    %x(1:3,t)=(xpre(1:3)+x(1:3,t-1))/2;
    %x(4:12,t)=xpre(4:12);
    
    
    %% Grafismos
+   figure(1);
+    hold on;
    clf;
    hold on;
    plot(xreal(1,t),xreal(2,t),'+');
--- Matlab/obs.m
+++ Matlab/obs.m
@@ -1,12 +1,6 @@
-function h=obs(x, L)
+function h=obs(x, L, areal)
 
     first= [x-L(1,:) ; x-L(2,:) ; x-L(3,:) ];
-    h = [ first(1,1)^2 + first(1,2)^2 ; first(2,1)^2 + first(2,2)^2 ; first(3,1)^2 + first(3,2)^2 ];
-    for k=1:1:length(h)
-       if(h(k,1)<1000000000000000)
-           h(k,1)=h(k,1)+rand(1,1)*0.2;
-       else
-           h(k,1)=10000000;   
-       end
-    end
+    h = [ first(1,1)^2 + first(1,2)^2 ; first(2,1)^2 + first(2,2)^2 ; first(3,1)^2 + first(3,2)^2 ; areal];
+
 end
